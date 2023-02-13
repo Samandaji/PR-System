ad89884b8259a0e183d0e19670300a0e4b7df981
samandaji <contactabubakarusman@gmail.com>
2023-02-13 12:38:28 +0100

    new update

---
+++ assets/boostrap/js/auth.js
@@ -0,0 +1,19 @@
+function validateFirstname(field)
+{
+  return (field == " ") ? "Enter FirstName please. \n" : ""
+}
+
+function validateOthername(field)
+{
+  return (field == " ") ? "Enter FirstName please. \n" : ""
+}
+
+function validaeLastname(field)
+{
+  return (field == " ") ? "Enter FirstName please. \n" : ""
+}
+
+function validateFirstname(field)
+{
+  return (field == " ") ? "Enter FirstName please. \n" : ""
+}
---
+++ register.php
@@ -0,0 +1,17 @@
+<?php
+//require_once('dBase.php');
+
+class Register{
+
+  public _Register()
+  {
+    return $empty;
+  }
+
+  public function sanitize($pdo, $var)
+  {
+
+  }
+}
+
+?>
