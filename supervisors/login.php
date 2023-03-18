5819aef6185040f2c681ed45c8bc2d83be48cccf
auwal00836 <auwalshikutu@gmail.com>
2023-03-18 03:18:21 +0100

    supervisors module creation

---
+++ supervisors/login.php
@@ -0,0 +1,111 @@
+<?php 
+  $msg = '';
+  $msg_type = '';
+
+  if (isset($_POST['login'])) 
+  {
+
+    require_once "supervisor.class.php";
+
+    // validate the user's inputs
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+  
+    $staff = new Staff();
+    $login = $staff->login($email, $password);
+
+    if (!$login) 
+    {
+
+      $msg_type = 'danger';
+      $msg = 'Failed to login.';
+      
+    }
+    else
+    {
+
+      $msg_type = 'success';
+      $msg = 'Login successfully.';
+      header("Location: dashboard.php" );
+
+    }
+    
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Buk PR System</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="../template/vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="../template/vendors/base/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="../template/css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="./img/buk_logo.jpg" />
+</head>
+
+<body>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-center auth px-0">
+        <div class="row w-100 mx-0">
+          <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+              <div class="brand-logo">
+                <center><img src="./img/buk_logo.jpg" alt="logo" height="120" style=" width:100px;"></center>
+              </div>
+              <h4 style="text-align: center;">Software Engeeneiring Department</h4>
+              <h6 class="font-weight-light">Sign in to continue.</h6>
+              <div class="alert alert-<?php echo $msg_type; ?>">
+                <?php echo $msg; ?>
+              </div>
+              <form class="pt-3" method="POST" action="">
+                <div class="form-group">
+                  <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username" name="email">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password" name="password">
+                </div>
+                <div class="mt-3">
+                  <button type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" name="login">SIGN IN</button>
+                </div>
+                <div class="my-2 d-flex justify-content-between align-items-center">
+                  <div class="form-check">
+                    <label class="form-check-label text-muted">
+                      <input type="checkbox" class="form-check-input">
+                      Keep me signed in
+                    </label>
+                  </div>
+                </div>
+                <div class="text-center mt-4 font-weight-light">
+                  Don't have an account? <a href="register.php" class="text-primary">Create</a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="../template/vendors/base/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- inject:js -->
+  <script src="../template/js/off-canvas.js"></script>
+  <script src="../template/js/hoverable-collapse.js"></script>
+  <script src="../template/js/template.js"></script>
+  <!-- endinject -->
+</body>
+
+</html>
---
+++ supervisors/sidebar.php
@@ -0,0 +1,47 @@
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="dashboard.php">
+              <i class="mdi mdi-home menu-icon"></i>
+              <span class="menu-title">Dashboard</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link"  href="view-submitions.php" >
+              <i class="mdi mdi-circle-outline menu-icon"></i>
+              <span class="menu-title">View Submitions</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="manage-projects.php">
+              <i class="mdi mdi-view-headline menu-icon"></i>
+              <span class="menu-title">Manage Student Projects</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="allocate-project.php">
+              <i class="mdi mdi-chart-pie menu-icon"></i>
+              <span class="menu-title">Allocate Project Topic</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="schedule-appointment.php">
+              <i class="mdi mdi-clock menu-icon"></i>
+              <span class="menu-title">Schedule Appointment</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="share-resources.php">
+              <i class="mdi mdi-emoticon menu-icon"></i>
+              <span class="menu-title">Share Resources</span>
+            </a>
+          </li>
+           <li class="nav-item">
+            <a class="nav-link" href="manage-result.php">
+              <i class="mdi mdi-pen menu-icon"></i>
+              <span class="menu-title">Compute And Manage Result</span>
+            </a>
+          </li>
+         
+        </ul>
+      </nav>
